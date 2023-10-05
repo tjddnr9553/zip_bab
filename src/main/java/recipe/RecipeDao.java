4af2928f149fff1460d2efa437e7e32877a33e38
Cha <tjddnr9553@naver.com>
2023-10-05 15:30:46 +0900

    레시피 VO,DAO,SERVICE (언더바 삭제)
    레시피 VO,DAO,SERVICE (언더바 삭제) 작업 수행

--- src/main/java/recipe/RecipeDao.java
+++ src/main/java/recipe/RecipeDao.java
@@ -9,20 +9,20 @@ import java.util.ArrayList;
 @Mapper
 public interface RecipeDao {
     //레시피 번호로 검색
-    @Select("Select * from Recipe where recipe_id=#{recipe_id}")
-    Recipe select(@Param("recipe_id") int recipe_id);
+    @Select("Select * from \"Recipe\" where \"recipeId\"=#{recipeId}")
+    Recipe select(@Param("recipeId") int recipeId);
 
     //레시피 전체 목록 출력
-    @Select("Select * from Recipe")
+    @Select("Select * from \"Recipe\"")
     ArrayList<Recipe> selectAll();
 
     //요리제목으로 검색
-    @Select("Select * from Recipe where title in #{title}")
+    @Select("Select * from \"Recipe\" where \"title\" in #{title}")
     ArrayList<Recipe> selectByTitle(@Param("title") String title);
 
     //재료정보로 검색
-    @Select("Select * from Recipe where ingredient_info in #{ingredient_info}")
-    ArrayList<Recipe> selectByIngredient_info(@Param("ingredient_info") String ingredient_info);
+    @Select("Select * from \"Recipe\" where \"ingredientInfo\" in #{ingredientInfo}")
+    ArrayList<Recipe> selectByIngredientInfo(@Param("ingredientInfo") String ingredientInfo);
 
 
 }
