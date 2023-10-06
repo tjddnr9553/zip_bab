57e55efe958529fe53ddbced988f3463f8b4401c
iblug <1115jkb@gmail.com>
2023-10-06 14:22:04 +0900

    [Add] Review CUD
    리뷰 작성, 수정 삭제 기능 초안

--- src/main/java/review/handler/ReviewEditHandler.java
+++ src/main/java/review/handler/ReviewEditHandler.java
@@ -6,20 +6,21 @@ import review.ReviewService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
 
-public class ReviewListHandler implements Handler {
+public class ReviewEditHandler implements Handler {
     @Override
     public String process(HttpServletRequest request, HttpServletResponse response) {
         String view = "/index.jsp";
         ReviewService service = new ReviewService();
         if (request.getMethod().equals("GET")) {
-            ArrayList<Review> list = service.getAll();
-            Review r = list.get(0);
-            request.setAttribute("r", r);
+            request.setAttribute("view", "/review/edit.jsp");
 
-//            request.setAttribute("view", "responsebody/member/edit.jsp");
-            return "responsebody/" + r.toString();
+        } else {
+            int reviewId = Integer.parseInt(request.getParameter("reviewId"));
+            String content = request.getParameter("content");
+
+            Review r = new Review(reviewId, 0, 0, content, null);
+            service.editReview(r);
         }
         return view;
     }
