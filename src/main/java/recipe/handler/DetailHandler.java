f0e486af12283fa506217b2fedf3d6badd69d852
Cha <tjddnr9553@naver.com>
2023-10-10 17:48:11 +0900

    Recipe detail 및 list 작업
    Recipe list VIEW단 카드형태로 변환
    Recipe detail 재료항목 및 열량 추가

--- src/main/java/recipe/handler/DetailHandler.java
+++ src/main/java/recipe/handler/DetailHandler.java
@@ -20,11 +20,58 @@ public class DetailHandler implements Handler {
         ReviewService rService = new ReviewService();
 
         Recipe r = service.getById(id);
+        String input = r.getIngredientInfo();
+
+        ArrayList<String> ingredient = new ArrayList<>();
+
+        if (input.startsWith("●")) {
+            String[] splitInput = input.split("●");
+            for (String str : splitInput) {
+                if (!str.trim().isEmpty()) {
+                    String[] colonSplit = str.split(":");
+                    if (colonSplit.length > 1) {
+                        String[] commaSplit = colonSplit[1].split(",");
+                        for (String s : commaSplit) {
+                            ingredient.add(s.trim());
+                        }
+                    }
+                }
+            }
+        } else if (input.startsWith("•")) {
+            String[] splitInput = input.split("•");
+            for (String str : splitInput) {
+                if (!str.trim().isEmpty()) {
+                    String[] colonSplit = str.split(":");
+                    if (colonSplit.length > 1) {
+                        String[] commaSplit = colonSplit[1].split(",");
+                        for (String s : commaSplit) {
+                            ingredient.add(s.trim());
+                        }
+                    }
+                }
+            }
+        }
+//        else if (input.startsWith("")) {
+//            String[] splitInput = input.split("");
+//            for (String str : splitInput) {
+//                if (!str.trim().isEmpty()) {
+//                    String[] colonSplit = str.split(":");
+//                    if (colonSplit.length > 1) {
+//                        String[] commaSplit = colonSplit[1].split(",");
+//                        for (String s : commaSplit) {
+//                            ingredient.add(s.trim());
+//                        }
+//                    }
+//                }
+//            }
+//        }
+
         ArrayList<ReviewMember> reviews = rService.getReviewMember(id);
 
-        request.setAttribute("r",r);
+        request.setAttribute("ingredient", ingredient);
+        request.setAttribute("r", r);
         request.setAttribute("reviews", reviews);
-        request.setAttribute("view","/recipe/detail.jsp");
+        request.setAttribute("view", "/recipe/detail.jsp");
         return view;
     }
 }
